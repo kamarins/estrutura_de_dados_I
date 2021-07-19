9250ba77338de2fbeca0f01db238d27db82d8f1e
kamarins <karineop@outlook.com>
2021-07-19 15:13:54 -0300

    aula7

---
+++ praticas/aula7/Casos de Teste - Aula Pratica 7 - Ordenacao/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 18/07/2021 20:25:34 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: Aula Prática 7 - Ordenação
---
+++ praticas/aula7/ordenacao.c
@@ -0,0 +1,45 @@
+#include "ordenacao.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Manter como especificado
+TipoAluno *alocaAlunos(TipoAluno *alunos, int n){
+
+  alunos = (TipoAluno*) malloc (sizeof(TipoAluno) * n);
+
+  return alunos;
+}
+
+//Manter como especificado
+TipoAluno *desalocaAlunos(TipoAluno *alunos){
+
+  free(alunos);
+  alunos = NULL;
+  
+  return alunos;
+}
+
+//ordena em ordem alfabetica
+void ordena(TipoAluno *alunos, int n){
+  int aux;//aux;
+  char armazena[20];
+
+  for (int i = 0; i < n; i++){
+  
+    aux = i;
+  
+    for (int j = i+1; j < n; j++){
+      if (strcmp(alunos[j].nome,alunos[aux].nome) < 0){
+        aux = j;
+      }
+    }
+
+    strcpy(armazena,alunos[aux].nome);
+    strcpy(alunos[aux].nome,alunos[i].nome);
+    strcpy(alunos[i].nome, armazena);
+  
+  }
+}
+
+
